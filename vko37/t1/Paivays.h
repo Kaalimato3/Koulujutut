6fc99fc77bd8f4200edf5b222e774f214d300655
Simo Bergius <simo.bergius@eng.tamk.fi>
2017-09-14 12:51:22 +0300

    t1

---
+++ vko37/t1/Paivays.h
@@ -0,0 +1,40 @@
+#pragma once
+/* 
+// #pragma once toimii Visual-ympäristössä, 
+// jossain muussa ympäristössä käytä kommentoitua menetelmää
+#ifndef PAIVAYS_H
+#define PAIVAYS_H
+// Lopussa #endif
+*/
+class Paivays
+{
+public:
+
+	Paivays();
+	Paivays(int vuosi, int kk, int paiva);
+	~Paivays();
+
+	// setters: (kirjoitusrutiinit)
+
+	void asetaVuosi(int vuosi);
+	void asetaKuukausi(int kk);
+	void asetaPaiva(int paiva);
+
+	// getters: (lukurutiinit)
+
+	int annaPaiva();
+	int annaKuukausi();
+	int annaVuosi();
+
+	void tulosta();
+
+private:
+
+	int paiva_;
+	int kk_;
+	int vuosi_;
+	// alaviivalla tunnistetaan attribuutit
+
+};
+
+//#endif
